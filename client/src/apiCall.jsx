85d9a00a212a86c10c9aa5cf3330349e47f28d60
Aditya Sanil <aditya.sanil@somaiya.edu>
2020-04-05 00:09:38 +0530

    Few error fixes before k8s test

--- client/src/apiCall.jsx
+++ client/src/apiCall.jsx
@@ -4,18 +4,17 @@ import axios from "axios";
 class Api extends Component {
   state = {
     data: "",
-    surname: ""
+    surname: "",
   };
 
   async componentDidMount() {
     const { data } = await axios.get("/api/name");
     const result = await axios.get("/api2/surname");
-    console.log(result);
     this.setState({ data: data.data, surname: result.data.surname });
   }
 
   render() {
-    return <Fragment>Name:</Fragment>;
+    return <Fragment>Name:{this.state.data}</Fragment>;
   }
 }
 
